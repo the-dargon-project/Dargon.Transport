5175adb22829bc501f123912ea8cf4300770ad2b
Michael Yu <ItzWarty@gmail.com>
2014-07-05 01:43:30 -0700

    Improved temporary logger to actually write stuff to console.

--- __DummyLoggerThisIsHorrible.cs
+++ __DummyLoggerThisIsHorrible.cs
@@ -1,9 +1,17 @@
-﻿namespace Dargon.Transport
+﻿using System;
+
+namespace Dargon.Transport
 {
    public static class __DummyLoggerThisIsHorrible
    {
       public static void L(LoggerLevel ll, params object[] herps)
       {
+         if (ll == LoggerLevel.Error)
+            Console.ForegroundColor = ConsoleColor.Red;
+         else if (ll == LoggerLevel.Info)
+            Console.ForegroundColor = ConsoleColor.Yellow;
+         foreach (var i in herps)
+            Console.WriteLine(herps);
       }
    }
 
