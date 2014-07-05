12be1341e46681fe54d5729b43f9c811205415ec
Michael Yu <ItzWarty@gmail.com>
2014-07-05 12:44:47 -0700

    Add libdtp unit test project and stub integration test.

---
+++ libdtp.Tests/DtpNodeIT.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Dargon.Transport;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace libdtp.Tests
+{
+   [TestClass]
+   public class DtpNodeIT
+   {
+      private DtpNode m_dtpNode;
+
+      [TestInitialize]
+      public void Setup()
+      {
+
+      }
+
+      [TestMethod]
+      public void TestMethod1()
+      {
+      }
+   }
+}
